6d6b6e963881a75ac788f03f531a82436610107a
sofiyareverse <sofiya.yurova@gmail.com>
2024-01-11 14:34:02 +0200

    fix create file service

--- app/services/programming_languages/create_prepared_data_file_service.rb
+++ app/services/programming_languages/create_prepared_data_file_service.rb
@@ -4,29 +4,31 @@ module ProgrammingLanguages
   class CreatePreparedDataFileService < ApplicationService
     def call
       programming_languages = ::ParseJsonService.call('data.json')
-      prepare_data(programming_languages)
-      create_new_file_with_prepared_data
+      create_file_with_data(prepare_data(programming_languages))
     end
 
     private
 
     def prepare_data(programming_languages)
-      @prepared_data = {}
+      prepared_data = {}
 
       programming_languages.each_with_index do |language, index|
         language_info = language.fetch_values('Name', 'Type', 'Designed by').join(', ').downcase
-        language_info.split(/,\s*/).each { |word| add_value(@prepared_data, word, index) }
-        if language_info.split(/,\s*|\s+/).count > 1
-          language_info.split(/,\s*|\s+/).each { |word| add_value(@prepared_data, word, index) }
-        end
+        add_words_to_data(prepared_data, language_info, index)
       end
 
-      @prepared_data
+      prepared_data
     end
 
-    def create_new_file_with_prepared_data
+    def add_words_to_data(hash, language_info, index)
+      language_info.split(/,\s*|\s+/).each do |word|
+        add_value(hash, word, index)
+      end
+    end
+
+    def create_file_with_data(data)
       File.open('search_programming_language_list.json', 'w') do |f|
-        f.write(@prepared_data.to_json)
+        f.write(data.to_json)
       end
     end
 
